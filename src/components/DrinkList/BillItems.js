bd6d3ed3a60e9bdfb712f4a9fbe476669fe1dc81
minhduong200xx <77377938+minhduong200xx@users.noreply.github.com>
2023-03-29 11:45:20 +0700

    completed

--- src/components/DrinkList/BillItems.js
+++ src/components/DrinkList/BillItems.js
@@ -22,7 +22,7 @@ const BillItems = ({ name, quantity, price, id, hide }) => {
   };
   return (
     <tr className="flex mx-auto w-[90%] h-6 py-2">
-      <td className="w-1/2 overflow-hidden h-6 pl-2">{name}</td>
+      <td className="w-1/2 overflow-hidden h-5 pl-2">{name}</td>
       <td className="w-1/6 justify-center flex ">
         <button
           className={`rounded font-bold w-5 h-5 border border-solid border-black flex items-center justify-center hover:bg-red-500 hover:text-white text-red-500 ${
